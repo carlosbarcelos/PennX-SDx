0c57bfc61f3cde277abdad32af9718884481ca1d
Carlos Barcelos <barcelosc96@gmail.com>
2017-06-15 15:45:09 -0400

    Completed Assignment 2: Squarelotron

--- SD1x/Assignments/Squarelotron.java
+++ SD1x/Assignments/Squarelotron.java
@@ -1,6 +1,6 @@
 package Assignments;
 
-public class Squarelotron {
+public class Squarelotron  {
 	int [][] squarelotron;
 	int size;
 
@@ -15,31 +15,126 @@ public class Squarelotron {
 			}
 		}
 	}
-	
-	/* This method performs the Upside-Down Flip of the squarelotron, as described above, and returns the new squarelotron.
-	 * The original squarelotron should not be modified. */
-	public Squarelotron upsideDownFlip(int ring){
-		// TODO
+
+	/* Creates a replica squarelotron */
+	private Squarelotron copySquarelotron(){
+		Squarelotron returnSquarelotron = new Squarelotron(this.size);
+		for(int i = 0; i < size; i ++){
+			for(int j = 0; j < size; j ++){
+				returnSquarelotron.squarelotron[i][j] = this.squarelotron[i][j];
+			}
+		}
+		return returnSquarelotron;
 	}
 	
+	/* Equals method for testing */
+	public boolean equals(Object obj){
+		Squarelotron s = (Squarelotron) obj;
+		for(int i = 0; i < size; i ++){
+			for(int j = 0; j < size; j ++){
+				if(this.squarelotron[i][j] != s.squarelotron[i][j]){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/* This method performs the Upside-Down Flip of the squarelotron, as described above, and returns the new squarelotron.
+	 * The original squarelotron should not be modified. */
+	public Squarelotron upsideDownFlip(int ring) {
+		//check that the ring specified is not out of bounds
+		int checkRings = (int) Math.ceil(size/2.0);
+		if(ring <= 0 || ring > checkRings){
+			return this;
+		}
+
+		//perform the upsideDownFlip
+		Squarelotron returnSquarelotron = this.copySquarelotron();
+		int maxDepth = (int) Math.floor(size/2.0);
+
+		for(int i = 0; i < maxDepth; i ++){
+			for(int j = 0; j < size; j ++){
+				if(i == ring - 1 ){ //top
+					if(j >= ring - 1 && j <= size - ring){
+						returnSquarelotron.squarelotron[i][j] = this.squarelotron[size-1-i][j];
+						returnSquarelotron.squarelotron[size-1-i][j] = this.squarelotron[i][j];
+					}
+				}
+				else if(i >= ring - 1 ){ //in middle range horizontally
+					if(j == ring - 1 || j == size - ring){ //in range vertically
+						returnSquarelotron.squarelotron[i][j] = this.squarelotron[size-1-i][j];
+						returnSquarelotron.squarelotron[size-1-i][j] = this.squarelotron[i][j];
+					}
+				}
+
+			}
+		}
+
+		//return the upsideDownFlip Squarelotron
+		return returnSquarelotron;
+	}
+
 	/* This method performs the Main Diagonal Flip of the squarelotron, as described above, and returns the new squarelotron.
 	 * The original squarelotron should not be modified. */
 	public Squarelotron mainDiagonalFlip(int ring){
-		// TODO
+		//check that the ring specified is not out of bounds
+		int checkRings = (int) Math.ceil(size/2.0);
+		if(ring <= 0 || ring > checkRings){
+			return this;
+		}
+
+		//perform the upsideDownFlip
+		Squarelotron returnSquarelotron = this.copySquarelotron();
+		int maxDepth = (int) Math.floor(size/2.0);
+
+		for(int i = 0; i < size; i ++){
+			for(int j = 0; j < size; j ++){
+				if(i == ring - 1 || i == size - ring){ //top&bottom
+					if(j >= ring - 1 && j <= size - ring){
+						returnSquarelotron.squarelotron[i][j] = this.squarelotron[j][i];
+					}
+				}
+				else if(i >= ring - 1 && i <= size - ring){ //in middle range horizontally
+					if(j == ring - 1 || j == size - ring){ //in range vertically
+						returnSquarelotron.squarelotron[i][j] = this.squarelotron[j][i];
+					}
+				}
+
+			}
+		}
+
+		//return the upsideDownFlip Squarelotron
+		return returnSquarelotron;
 	}
-	
+
 	/* The argument numberOfTurns indicates the number of times the entire squarelotron should be rotated 90° clockwise.
 	 * Any integer, including zero and negative integers, is allowable as the argument.
 	 * A value of -1 indicates a 90° counterclockwise rotation.
 	 * This method modifies the internal representation of the squarelotron; it does not create a new squarelotron. */
 	public void rotateRight(int numberOfTurns){
-		// TODO
+		if (numberOfTurns == 0 || numberOfTurns%4==0){
+			return;
+		}
+		int[][] newSquarelotron = new int[size][size];
+		if(numberOfTurns >= 1){
+			for(int thisMany = 0; thisMany < numberOfTurns; thisMany++){
+				for(int i = 0; i < size; i ++){
+					for(int j = 0; j < size; j ++){
+						newSquarelotron[i][j] = this.squarelotron[size - j - 1][i];
+					}
+				}
+			}
+		}
+		if(numberOfTurns <= -1){
+			for(int thisMany = 0; thisMany < Math.abs(numberOfTurns); thisMany++){
+				for(int i = 0; i < size; i ++){
+					for(int j = 0; j < size; j ++){
+						newSquarelotron[i][j] = this.squarelotron[j][size - i - 1];
+					}
+				}
+			}
+		}
+		this.squarelotron = newSquarelotron;
 	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
--- SD1x/Assignments/SquarelotronTest.java
+++ SD1x/Assignments/SquarelotronTest.java
@@ -6,24 +6,149 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SquarelotronTest {
+	Squarelotron s2;
+	Squarelotron s3;
 
 	@Before
 	public void setUp() throws Exception {
+		s2 = new Squarelotron(4);
+		s3 = new Squarelotron(5);
 	}
-
+	
+	@Test
+	public void testBuildSquarelotron() {
+		Squarelotron mockS2 = new Squarelotron(4); //test an even number of rings
+		int[] mockS2Row1 = {1,2,3,4};
+		int[] mockS2Row2 = {5,6,7,8};
+		int[] mockS2Row3 = {9,10,11,12};
+		int[] mockS2Row4 = {13,14,15,16};
+		mockS2.squarelotron[0] = mockS2Row1;
+		mockS2.squarelotron[1] = mockS2Row2;
+		mockS2.squarelotron[2] = mockS2Row3;
+		mockS2.squarelotron[3] = mockS2Row4;
+		assertTrue(mockS2.equals(s2));
+		
+		Squarelotron mockS3 = new Squarelotron(5); //test an odd number of rings
+		int[] mockS3Row1 = {1,2,3,4,5};
+		int[] mockS3Row2 = {6,7,8,9,10};
+		int[] mockS3Row3 = {11,12,13,14,15};
+		int[] mockS3Row4 = {16,17,18,19,20};
+		int[] mockS3Row5 = {21,22,23,24,25};
+		mockS3.squarelotron[0] = mockS3Row1;
+		mockS3.squarelotron[1] = mockS3Row2;
+		mockS3.squarelotron[2] = mockS3Row3;
+		mockS3.squarelotron[3] = mockS3Row4;
+		mockS3.squarelotron[4] = mockS3Row5;
+		assertTrue(mockS3.equals(s3));
+	}
+	
 	@Test
 	public void testUpsideDownFlip() {
-		fail("Not yet implemented");
+		Squarelotron mockS2 = new Squarelotron(4); //test an even number of rings
+		int[] mockS2Row1 = {13,14,15,16};
+		int[] mockS2Row2 = { 9, 6, 7,12};
+		int[] mockS2Row3 = { 5,10,11, 8};
+		int[] mockS2Row4 = { 1, 2, 3, 4};
+		mockS2.squarelotron[0] = mockS2Row1;
+		mockS2.squarelotron[1] = mockS2Row2;
+		mockS2.squarelotron[2] = mockS2Row3;
+		mockS2.squarelotron[3] = mockS2Row4;
+		assertTrue(mockS2.equals(s2.upsideDownFlip(1)));
+		
+		Squarelotron mockS3 = new Squarelotron(5); //test an odd number of rings
+		int[] mockS3Row1 = { 1, 2, 3, 4, 5};
+		int[] mockS3Row2 = { 6,17,18,19,10};
+		int[] mockS3Row3 = {11,12,13,14,15};
+		int[] mockS3Row4 = {16, 7, 8, 9,20};
+		int[] mockS3Row5 = {21,22,23,24,25};
+		mockS3.squarelotron[0] = mockS3Row1;
+		mockS3.squarelotron[1] = mockS3Row2;
+		mockS3.squarelotron[2] = mockS3Row3;
+		mockS3.squarelotron[3] = mockS3Row4;
+		mockS3.squarelotron[4] = mockS3Row5;
+		assertTrue(mockS3.equals(s3.upsideDownFlip(2)));
 	}
 
 	@Test
 	public void testMainDiagonalFlip() {
-		fail("Not yet implemented");
+		Squarelotron mockS2 = new Squarelotron(4); //test an even number of rings
+		int[] mockS2Row1 = { 1, 5, 9,13};
+		int[] mockS2Row2 = { 2, 6, 7,14};
+		int[] mockS2Row3 = { 3,10,11,15};
+		int[] mockS2Row4 = { 4, 8,12,16};
+		mockS2.squarelotron[0] = mockS2Row1;
+		mockS2.squarelotron[1] = mockS2Row2;
+		mockS2.squarelotron[2] = mockS2Row3;
+		mockS2.squarelotron[3] = mockS2Row4;
+		assertTrue(mockS2.equals(s2.mainDiagonalFlip(1)));
+		
+		Squarelotron mockS3 = new Squarelotron(5); //test an odd number of rings
+		int[] mockS3Row1 = { 1, 2, 3, 4, 5};
+		int[] mockS3Row2 = { 6, 7,12,17,10};
+		int[] mockS3Row3 = {11, 8,13,18,15};
+		int[] mockS3Row4 = {16, 9,14,19,20};
+		int[] mockS3Row5 = {21,22,23,24,25};
+		mockS3.squarelotron[0] = mockS3Row1;
+		mockS3.squarelotron[1] = mockS3Row2;
+		mockS3.squarelotron[2] = mockS3Row3;
+		mockS3.squarelotron[3] = mockS3Row4;
+		mockS3.squarelotron[4] = mockS3Row5;
+		assertTrue(mockS3.equals(s3.mainDiagonalFlip(2)));
 	}
 
 	@Test
 	public void testRotateRight() {
-		fail("Not yet implemented");
+		Squarelotron mockS2 = new Squarelotron(4); //test an even number of rings
+		int[] mockS2Row1 = {1,2,3,4};
+		int[] mockS2Row2 = {5,6,7,8};
+		int[] mockS2Row3 = {9,10,11,12};
+		int[] mockS2Row4 = {13,14,15,16};
+		mockS2.squarelotron[0] = mockS2Row1;
+		mockS2.squarelotron[1] = mockS2Row2;
+		mockS2.squarelotron[2] = mockS2Row3;
+		mockS2.squarelotron[3] = mockS2Row4;
+		
+		Squarelotron rightS2 = new Squarelotron(4); //test an even number of rings
+		int[] rightS2Row1 = {13,9,5,1};
+		int[] rightS2Row2 = {14,10,6,2};
+		int[] rightS2Row3 = {15,11,7,3};
+		int[] rightS2Row4 = {16,12,8,4};
+		rightS2.squarelotron[0] = rightS2Row1;
+		rightS2.squarelotron[1] = rightS2Row2;
+		rightS2.squarelotron[2] = rightS2Row3;
+		rightS2.squarelotron[3] = rightS2Row4;
+		
+		Squarelotron mockS3 = new Squarelotron(5); //test an odd number of rings
+		int[] mockS3Row1 = {1,2,3,4,5};
+		int[] mockS3Row2 = {6,7,8,9,10};
+		int[] mockS3Row3 = {11,12,13,14,15};
+		int[] mockS3Row4 = {16,17,18,19,20};
+		int[] mockS3Row5 = {21,22,23,24,25};
+		mockS3.squarelotron[0] = mockS3Row1;
+		mockS3.squarelotron[1] = mockS3Row2;
+		mockS3.squarelotron[2] = mockS3Row3;
+		mockS3.squarelotron[3] = mockS3Row4;
+		mockS3.squarelotron[4] = mockS3Row5;
+		//no rotation
+		s2.rotateRight(0);
+		assertTrue(mockS2.equals(s2));
+		s2.rotateRight(4);
+		assertTrue(mockS2.equals(s2));
+		
+		s3.rotateRight(0);
+		assertTrue(mockS3.equals(s3));
+		s3.rotateRight(4);
+		assertTrue(mockS3.equals(s3));
+
+		//rotation
+		s2.rotateRight(1);
+		assertTrue(rightS2.equals(s2));
+		s2.rotateRight(-1);
+		assertTrue(mockS2.equals(s2));
+		
+		s3.rotateRight(1);
+		s3.rotateRight(-1);
+		assertTrue(mockS3.equals(s3));
 	}
 
 }
